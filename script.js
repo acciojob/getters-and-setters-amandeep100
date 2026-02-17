1a0cfcc2aeecaacc400e11e89add13dc26b91245
Amandeep singh <amansingh765487@gmail.com>
2026-02-17 21:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+this.name = name
+	this.age = age;
+	}
+	get name(){
+return this.name;
+	}
+	get age(){
+return this.age;
+	}
+	set age(newAge){
+this.age = newAge
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+study(){
+	console.log(`${this.name} is teaching`)
+
+}
+	
+}
 
 class Teacher extends Person {}
 
